b62ea5f5373bfe13489f04777e3ecf6e1d4999da
Isabella Nigro <isabellanigro@Isabellas-MacBook-Pro.local>
2019-10-10 17:48:10 -0300

    fixing files

---
+++ src/components/boxprofile.js
@@ -0,0 +1,8 @@
+import React from 'react';
+import boxprofile from './boxprofile.svg'
+const boxprofile = () => (
+    <div className="boxprofile">
+      <img src={boxprofile} />
+    </div>
+  )
+export {boxprofile}
