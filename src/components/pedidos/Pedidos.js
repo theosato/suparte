5f9fc93dbbe302d0c10944321de6fe1abdca5583
guilhermeyuiti <guilhermee.yuiti@gmail.com>
2019-11-05 00:02:19 -0300

    Ok

---
+++ src/components/pedidos/Pedidos.js
@@ -0,0 +1,79 @@
+import React, { Component } from 'react';
+import './Pedidos.css';
+import { Back } from './components/Back';
+import { Search } from './components/Search';
+import { BottomBarPerfil } from './components/bottomBarPerfil/BottomBarPerfil';
+import { Pedido } from './components/pedido/Pedido';
+import { BottomApp } from '../bottomApp/BottomApp';
+import { HomeHeader } from '../homeHeader/HomeHeader';
+import { PedidoHeader } from '../pedidoHeader/PedidoHeader';
+import { ItemPedido } from '../itemPedido/ItemPedido';
+import { Link } from 'react-router-dom';
+
+function Pedidos() {
+
+  const pedidos = [
+    {
+      id: '1970433',
+      date: "12/05/2019",
+      tracking_code: "IW294295I23",
+      quantity: "2",
+      total: "R$112,21",
+      status: "Entregue"
+    },
+    {
+      id: '23124242',
+      date: "12/05/2019",
+      tracking_code: "IW294195I23",
+      quantity: "1",
+      total: "R$112,21",
+      status: "Entregue"
+    },
+    {
+      id: '12412412',
+      date: "12/05/2019",
+      tracking_code: "BC294295I23",
+      quantity: "1",
+      total: "R$112,21",
+      status: "Entregue"
+    }
+  ]
+
+  return (
+    <div className="conteiner">
+      <PedidoHeader />
+      <div className="MeusPedidos">Meus Pedidos</div>
+      <table className="Options">
+        <tr>
+          <td className="Option">
+            <div className="Entregues">Entregues</div>
+          </td>
+          <td className="Option">
+            <div className="Processando">Processando</div>
+          </td>
+          <td className="Option">
+          <div className="Cancelados">Cancelados</div>
+          </td>
+        </tr>
+      </table>
+
+      <div className="Pedidos"> 
+          { pedidos.map (
+            item => 
+             <ItemPedido 
+              id={item.id}
+              date={item.date}
+              tracking_code={item.tracking_code} 
+              quantity={item.quantity}
+              total={item.total}
+              status={item.status} 
+            /> 
+          )}
+        </div>
+
+        <BottomApp />
+      </div>
+  )
+}
+
+export default Pedidos;
--- src/components/product/Product.js
+++ src/components/product/Product.js
@@ -3,8 +3,41 @@ import './Product.css';
 import { BottomApp } from '../bottomApp/BottomApp';
 import { ProductHeader } from '../productHeader/ProductHeader';
 import queryString from 'query-string';
+import { Rating } from '../rating/Rating';
 
+<<<<<<< Updated upstream
 const Product = ({id, photo, price, name, description, author, location}) => {
+=======
+const Product = () => {
+    var produto = {}
+
+    const items = [
+      {
+          prod_id: '1',
+          photo: 'https://www.thephotoargus.com/wp-content/uploads/2018/09/8uniquegift.jpg',
+          name: "Item 1",
+          author: "Author 1",
+          price: "R$40",
+          description: "That's to my cigarettes."
+      },
+      {
+          prod_id: '2',
+          photo: 'https://render.fineartamerica.com/images/rendered/search/print/images/artworkimages/medium/1/michael-stipe-richard-day.jpg',
+          name: "Item 2",
+          author: "Author 2",
+          price: "R$20",
+          description: "Please, buy it. I'm poor."
+      },
+      {
+          prod_id: '3',
+          photo: "https://i.pinimg.com/originals/6d/e3/da/6de3da7230d1797a11fc95ae26b31b80.jpg",
+          name: "Item 3",
+          author: "Author 3",
+          price: "R$30",
+          description: "That's a nice product."
+      }
+  ]
+>>>>>>> Stashed changes
 
   if(window){
     const values = queryString.parse(location.search)
@@ -15,12 +48,46 @@ const Product = ({id, photo, price, name, description, author, location}) => {
     <div className="Product">
       {console.log("entrou")}
       <ProductHeader />
+<<<<<<< Updated upstream
       <div className="Id">{ id }</div>
       <div className="Photo"><img src = { photo } width='100px' height='140px'/></div>
       <div className="Author">{ author }</div>
       <div className="Name">{ name }</div>
       <div className="Description">{ description }</div>
       <div className="Price">{ price }</div>
+=======
+      <div className="Id">{ produto.id }</div>
+      <div className="Photo"><img src = { produto.photo } width='375px' height='375px'/></div>
+      <div className="ProductSpace">
+        <table className="Table1">
+          <tr>
+            <td className="Option">
+              <div className="Name">{ produto.name }</div>
+            </td>
+            <td className="Option">
+              <div className="Price">{ produto.price }</div>
+            </td>
+          </tr>
+        </table>
+        <table className="Table2">
+          <tr>
+            <td className="Option">
+            <div className="Author">{ produto.author }</div>
+            </td>
+          </tr>
+          <tr>
+            <td className="Option">
+              <Rating />
+            </td>
+          </tr>
+          <tr>
+            <td className="Option">
+            <div className="Description">{ produto.description }</div>
+            </td>
+          </tr>
+        </table>
+      </div>
+>>>>>>> Stashed changes
       <BottomApp />
     </div>)
 }
